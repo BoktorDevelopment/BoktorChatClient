d08b0b841c9345af38b48552a0386374dd3e8266
BoktorDevelopment <kris.broekstra@outlook.com>
2018-01-05 16:30:25 +0100

    create2

---
+++ BoktorChatClient/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoktorChatClient
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream;
+        private string username;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+
+            //while (true)
+            //{
+            //    byte[] inStream = new byte[10025];
+            //    serverStream.Read(inStream, 0, inStream.Length);
+            //    string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            //    msg(returndata);
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, inStream.Length);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg(returndata);
+            textBox2.Text = "";
+            textBox2.Focus();
+        }
+
+        public void msg(string mesg)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            username = usernamebox.Text;
+
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            clientSocket.Close();
+        }
+    }
+}
+
